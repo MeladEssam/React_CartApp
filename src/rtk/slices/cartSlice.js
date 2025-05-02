b4b2b5554eb0004f929b01576f650ac947f1305c
MeladEssam <https://github.com/MeladEssam>
2025-05-02 04:14:35 +0300

    Fixing null value issue

--- src/rtk/slices/cartSlice.js
+++ src/rtk/slices/cartSlice.js
@@ -1,7 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 let cartSlice = createSlice({
-  initialState: JSON.parse(localStorage.getItem("cart")),
+  initialState: JSON.parse(localStorage.getItem("cart")) || [],
   name: "cartSlice",
   reducers: {
     addProductToCart: (state, action) => {
@@ -14,19 +14,14 @@ let cartSlice = createSlice({
         let newProduct = { ...action.payload, quantity: 1 };
         state.push(newProduct);
       }
-      // localStorage.setItem("cart_products", JSON.stringify(state));
-      // localStorage.setItem("cart", JSON.stringify(state));
     },
     deleteFromCart: (state, action) => {
       let newState = state.filter(
         (product) => product.id !== action.payload.id
       );
-      // localStorage.setItem("cart", JSON.stringify(newState));
-      // return state.filter((product) => product.id !== action.payload.id);
       return newState;
     },
     clearCart: () => {
-      // localStorage.clear();
       return [];
     },
   },
